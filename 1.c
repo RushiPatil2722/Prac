ae307a22991c266338e29cc92b598fe7a4387203
RushiPatil2722 <127736762+RushiPatil2722@users.noreply.github.com>
2023-03-26 23:15:08 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{   
+    int i,sum=0;
+    int arr[]={24,36,54,52,36,14,25,78};
+    for(i=0;i<7;i++)
+    {
+        sum=sum+arr[i];
+        
+    }
+    
+    printf("%d",sum);  
+    
+}
